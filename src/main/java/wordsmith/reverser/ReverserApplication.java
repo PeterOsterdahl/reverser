50f76490cb07dd4159e45f4edb7cd63f0c647624
daros <daros@kth.se>
2021-10-03 20:26:46 +0200

    Initial project setup

---
+++ src/main/java/wordsmith/reverser/ReverserApplication.java
@@ -0,0 +1,13 @@
+package wordsmith.reverser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReverserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReverserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wordsmith/reverser/ReverserApplicationTests.java
@@ -0,0 +1,13 @@
+package wordsmith.reverser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReverserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
